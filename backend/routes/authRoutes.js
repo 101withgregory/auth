fbbd017813db0a43d0c71bc913b1f55f00325fa8
101withgregory <101withgregoryonyando@gmail.com>
2025-11-13 21:06:22 +0300

    fixed routing problem

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -12,5 +12,5 @@ router.post('/logout', logout)
 router.post('/verify-email',verifyEmail )
 router.post('/forgot-password', forgotPassword)
 
-router.post('reset-password/:token', resetPassword)
+router.post('/reset-password/:token', resetPassword)
 export default router; 
